f50520cdade0099f1174217dca427cb0138743d0
maddinthe <martintheilen@gmail.com>
2015-11-25 11:37:42 +0100

    ue 25.11.15

--- unterricht/november/dynamischeWebsite/pages/formulare.php
+++ unterricht/november/dynamischeWebsite/pages/formulare.php
@@ -2,6 +2,7 @@
 
     <input type="text" placeholder="name" name="login"/><br/>
     <input type="password" placeholder="password" name="password"/><br/>
+    <input type="hidden" name="page" value="formulare"/>
     <button type="submit">Anmelden</button>
     <input type="reset" value="Reset"/>
 
